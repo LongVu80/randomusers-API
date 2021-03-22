ad812682aab121b6ab812ddca78631fe9e53fd4f
LongVu80 <thienlong14th@gmail.com>
2021-03-21 19:52:20 -0500

    updated my js file

--- js/random.js
+++ js/random.js
@@ -2,20 +2,13 @@ $(document).ready(function () {
     $.ajax({
         url: 'https://randomuser.me/api/?results=20', dataType: 'json', success: function (user) {
             console.log(user.results[0]);
-            for (var i = 0; i < 20; i++) {
+            var results= user.results
+            for (var i = 0; i < results.length; i++) {
                 $("#user").append("<tr>" +
-                    "<td>" +
-                    user.results[i].name.title + " " + user.results[i].name.first + " " + user.results[i].name.last +
-                    "</td>" +
-                    "<td>" +
-                    user.results[i].dob.age +
-                    "</td>" +
-                    "<td>" +
-                    user.results[i].email +
-                    "</td>" +
-                    "<td>" +
-                    `${user.results[i].location.street.number} ${user.results[i].location.street.name}, ${user.results[i].location.city}, ${user.results[i].location.state} ${user.results[i].location.postcode}` +
-                    "</td>" +
+                    "<td>" + user.results[i].name.title + " " + user.results[i].name.first + " " + user.results[i].name.last + "</td>" +
+                    "<td>" + user.results[i].registered.age + "</td>" +
+                    "<td>" + user.results[i].email + "</td>" +
+                    "<td>" + user.results[i].location.street.number + " " + user.results[i].location.street.name + " " + user.results[i].location.city + " " + user.results[i].location.state + " " + user.results[i].location.postcode + "</td>" +
                     "</tr>"
                 );
 
