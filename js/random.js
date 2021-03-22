8abbd9f81a71dab9c87193f4c00c9706276f1503
LongVu80 <thienlong14th@gmail.com>
2021-03-21 19:56:07 -0500

    update js file

--- js/random.js
+++ js/random.js
@@ -2,13 +2,13 @@ $(document).ready(function () {
     $.ajax({
         url: 'https://randomuser.me/api/?results=20', dataType: 'json', success: function (user) {
             console.log(user.results[0]);
-            var results= user.results;g
+            var results= user.results;
             for (var i = 0; i < results.length; i++) {
                 $("#user").append("<tr>" +
                     "<td>" + user.results[i].name.title + " " + user.results[i].name.first + " " + user.results[i].name.last + "</td>" +
                     "<td>" + user.results[i].registered.age + "</td>" +
                     "<td>" + user.results[i].email + "</td>" +
-                    "<td>" + user.results[i].location.street.number + " " + user.results[i].location.street.name + " " + user.results[i].location.city + " " + user.results[i].location.state + " " + user.results[i].location.postcode + "</td>" +
+                    "<td>" + user.results[i].location.street.number + " " + user.results[i].location.street.name + ", " + user.results[i].location.city + ", " + user.results[i].location.state + " " + user.results[i].location.postcode + "</td>" +
                     "</tr>"
                 );
 
